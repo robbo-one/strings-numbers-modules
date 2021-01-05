258d6102ce652f720c377a1907cbbf2c93c356fc
student <student@eda-bc-08.localdomain>
2021-01-05 16:42:37 +1300

    Answer Kata Strings Numbers Modules

--- utilities.js
+++ utilities.js
@@ -1,19 +1,39 @@
+module.exports = {
+  getType: getType,
+  isNumber: isNumber,
+  toNumber: toNumber,
+  isStringNumber: isStringNumber,
+  add: add,
+  addStrings: addStrings
+}
+
 function getType (thing) {
+  return(typeof thing)
 }
 
 function isNumber (thing) {
+  if (typeof thing != "number")
+  return false
+  else {
+    return true
+  }
 }
 
 function toNumber (str) {
+  return parseInt(str)
 }
 
 function isStringNumber (str) {
+  return !isNaN(str)
 }
 
 function add (a, b) {
+ return a + b
 }
 
 function addStrings (a, b) {
+  return a + parseInt(b)
+
 }
 
 function addStringsOrNumbers (a, b) {
