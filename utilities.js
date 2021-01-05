9103de67c68b3fae39cd12f44be4305fd3031d77
student <student@eda-bc-05.localdomain>
2021-01-05 16:46:18 +1300

    teamwork pt 2 this time it's personal

--- utilities.js
+++ utilities.js
@@ -1,19 +1,61 @@
+
+module.exports = {
+  getType: getType,
+  isNumber: isNumber,
+  toNumber: toNumber,
+  isStringNumber:isStringNumber,
+  add: add,
+  addStrings: addStrings,
+  addStringsOrNumbers: addStringsOrNumbers
+}
+
 function getType (thing) {
+
+  return typeof thing
+
+
 }
 
 function isNumber (thing) {
+
+if (typeof(thing) === "number"){
+return true}
+
+return false
 }
 
 function toNumber (str) {
+
+  return Number(str)
+
 }
 
 function isStringNumber (str) {
+
+if (typeof(str) === "string"){
+
+    if (Number(str)){
+      return true
+    }
+    return false
+}
+
 }
 
 function add (a, b) {
+
+  if (typeof(a) === "number" && typeof(b) === "number") {
+   
+    return a + b
+  } 
+    
 }
 
 function addStrings (a, b) {
+
+var answer = Number(a) + Number(b)
+
+return answer.toString()
 }
 
 function addStringsOrNumbers (a, b) {
