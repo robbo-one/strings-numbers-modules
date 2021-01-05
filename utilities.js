9d428acfba23ae282840b232bb6b5b0cfe7f1dc3
Michael Parkin <73506758+mike-parkin@users.noreply.github.com>
2021-01-05 15:04:55 +1300

    getType & isNumber both complete

--- utilities.js
+++ utilities.js
@@ -1,7 +1,32 @@
+module.exports = {
+  getType: getType,
+  isNumber: isNumber,
+  toNumber: toNumber,
+  isStringNumber: isStringNumber,
+  add: add,
+  addStrings: addStrings,
+  addStringsOrNumbers: addStringsOrNumbers,
+  isEmail: isEmail,
+  countIf: countIf,
+  filter: filter,
+  map: map,
+  filterStringsWithCommas: filterStringsWithCommas,
+  splitStringByCommas: splitStringByCommas
+}
+
+
 function getType (thing) {
+  return typeof thing
 }
 
 function isNumber (thing) {
+  // return typeof thing === Number
+  // if (thing.isNumber) {return true}
+  // else {return false}
+  if (typeof thing === "number") {
+    return true
+  }
+  return false
 }
 
 function toNumber (str) {
